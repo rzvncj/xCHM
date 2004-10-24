3ea03fffac2ef0a9e52be5cce6e64aff814377dc
Razvan Cojocaru <rzvncj@gmail.com>
2004-10-24 10:32:18 +0000

    TOC bugfix. 0.9.6 to be released.

--- src/contenttaghandler.cpp
+++ src/contenttaghandler.cpp
@@ -92,10 +92,7 @@ bool ContentTagHandler::HandleTag(const wxHtmlTag& tag)
 			_title = _url = wxEmptyString;
 			ParseInner(tag);
 
-			if(_title.IsEmpty())
-				_title = wxT("?");
-
-			if(_treeCtrl && !_url.IsEmpty()) {
+			if(_treeCtrl && !_title.IsEmpty()) {
 
 				_parents[_level] = 
 					_treeCtrl->AppendItem(

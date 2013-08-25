8f77edc01da13962bd7e61c4f39341fdfada3d28
Razvan Cojocaru <rzvncj@gmail.com>
2013-08-25 23:23:55 +0300

    Updated copyright year

--- src/chmindexpanel.h
+++ src/chmindexpanel.h
@@ -1,6 +1,6 @@
 /*
 
-  Copyright (C) 2003 - 2012  Razvan Cojocaru <rzvncj@gmail.com>
+  Copyright (C) 2003 - 2013  Razvan Cojocaru <rzvncj@gmail.com>
  
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
